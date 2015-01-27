9c0a9560d05e4d5402118f018357385a6ae6d8b0
Chubes <Chubes@andrew-mercers-macbook.local>
2015-01-27 16:51:55 -0500

    draft 1

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -4,42 +4,59 @@ require "httparty"
 require "pry"
 
 module Cheepcreep
+  class User < ActiveRecord::Base
+    validates :login, presence: true
+  end
 end
-
-  class User
-  end
-
-  class Github
-    include HTTParty
+class Github
+  include HTTParty
     base_uri 'https://api.github.com'
-  
-    resp = HTTParty.get(base_uri + '/users/redline6561')
-  
-    def initialize
-      # ENV["FOO"] is like echo $FOO
-      @auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
-    end
+    basic_auth ENV['GITHUB_USER'], ENV['GITHUB_PASS']
 
-    def get_followers(screen_name)
-      result = self.get("/users/#{screen_name}/followers")
+    #resp = HTTParty.get(base_uri + '/users/redline6561')
+
+    def get_user(user = 'redline6561')
+      result = self.class.get("/users/#{user}")
+      puts "#{result.headers['x-ratelimit-remaining']} requests left!"
       json = JSON.parse(result.body)
     end
 
-    def get_user(screen_name)
-      result = self.get("/users/#{screen_name}")
+    def get_followers(user = 'redline6561', page=1, per_page=20)
+      options = {:query => {:page => page, :per_page => per_page}}
+      result = self.class.get("/users/#{user}/followers", options)
+      puts "#{result.headers['x-ratelimit-remaining']} requests left!"
       json = JSON.parse(result.body)
+      followers_array
     end
 
-    def sort_users(screen_name)
-      self.get("/users/#{screen_name}")
-      json = JSON.parse(result.body)
-      User.order(followers: :asc)
-    end
-  end
+    #def sort_users(screen_name)
+    #  self.class.get("/users/#{screen_name}")
+    #  #json = JSON.parse(result.body)
+    #  User.order(followers: :asc)
+    #end
+end  
+def add_user(screen_name)
+  github = Github.new
+  result = github.class.get("/users/#{screen_name}")
+  json = JSON.parse(result.body)
+  user = github.get_user(screen_name)
+  Cheepcreep::User.create(:login => json['login'], 
+                          :name => json['name'], 
+                          :blog => json['blog'], 
+                          :public_repos => json['public_repos'], 
+                          :followers => json['followers'], 
+                          :following => json['following'])
+end 
+
+
+#add_user('redline6561')
+
+#followers = github.get_followers('redline6561', 1, 100)
+#followers.map { |x| x['login'] }.sample(20).each do |username|
+ # add_github_user(username)
+ # end
 
 binding.pry
 
-#github = Github.new
-#resp = github.get_followers('redline6561')
-#followers = JSON.parse(resp.body)
-#Cheepcreep::GithubUser.create
+
+

b1f0d3923e761953932d7c7fba251dd108ba2515
Chubes <Chubes@andrew-mercers-macbook.local>
2015-01-26 20:14:54 -0500

    With followers/users accessability

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -6,21 +6,40 @@ require "pry"
 module Cheepcreep
 end
 
-class Github
-  include HTTParty
-  base_uri 'https://api.github.com'
-
-  def initialize
-    # ENV["FOO"] is like echo $FOO
-    #@auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+  class User
   end
-end
 
+  class Github
+    include HTTParty
+    base_uri 'https://api.github.com'
+  
+    resp = HTTParty.get(base_uri + '/users/redline6561')
+  
+    def initialize
+      # ENV["FOO"] is like echo $FOO
+      @auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+    end
 
-class CheepcreepApp
-end
+    def get_followers(screen_name)
+      result = self.class.get("/users/#{screen_name}/followers")
+      json = JSON.parse(result.body)
+    end
+
+    def get_user(screen_name)
+      result = self.class.get("/users/#{screen_name}")
+      json = JSON.parse(result.body)
+    end
+
+    def sort_users(screen_name)
+      self.class.get("/users/#{screen_name}")
+      json = JSON.parse(result.body)
+
+    end
+  end
 
 binding.pry
 
-creeper = CheepcreepApp.new
-creeper.creep
+#github = Github.new
+#resp = github.get_followers('redline6561')
+#followers = JSON.parse(resp.body)
+#Cheepcreep::GithubUser.create
